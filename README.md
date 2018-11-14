7e9b907016946b1ab0953dee384a896b0e613fbf
lopezfjose <jflopezfernandez@gmail.com>
2018-11-14 17:42:27 -0500

    Tokenizing simple single-character expressions

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # SymCalc
-Symbolic calculator written in Haskell
+Symbolic calculator written in Haskell.
 
 ## Description
 This calculator is based on primarily on the one [written by](https://www.schoolofhaskell.com/school/starting-with-haskell/basics-of-haskell/3-pure-functions-laziness-io) the man, the myth, the legend, [Bartosz Milewski](https://bartoszmilewski.com/).
+
+At a very high level, the calculator is a loop that gets a line of text from the user and then calculates and displays the result.
+
+The calculation is done in three steps.
+
+1. Lexical analysis - The string is converted into tokens.
+2. Parsing - Building an expression tree.
+3. Evaluation - The expression is evaluated.
--- src/main.hs
+++ src/main.hs
@@ -1,30 +1,60 @@
 
-import System.Environment
 import Data.Char
-
 
 data Operator = Plus | Minus | Times | Div
         deriving (Show, Eq)
 
-operatorToString :: Operator -> String
-operatorToString Plus = "+"
-operatorToString Minus = "-"
-operatorToString Times = "*"
-operatorToString Div = "/"
-
-data Token = TokenOperator Operator
-           | TokenIdentifier String
-           | TokenNumber Int
+data Token = TokenOperator Operator 
+            | TokenIdentifier String 
+            | TokenNumber Int
+            | TokenSpace
         deriving (Show, Eq)
 
-showContent :: Token -> String
-showContent (TokenOperator op) = operatorToString
-showContent (TokenIdentifier str) = operatorToString
-showContent (TokenNumber i) = show i
+operator :: Char -> Operator
+operator c | c == '+'  = Plus
+           | c == '-'  = Minus
+           | c == '*'  = Times
+           | c == '/'  = Div
+           | otherwise = error "Other operator"
+
+tokenize :: String -> [Token]
+tokenize = map tokenizeChar
+
+tokenizeChar :: Char -> Token
+tokenizeChar c | elem c "+-*/" = TokenOperator (operator c)
+               | isDigit c     = TokenNumber (digitToInt c)
+               | isAlpha c     = TokenIdentifier [c]
+               | isSpace c     = TokenSpace
+               | otherwise     = error $ "Cannot tokenize " ++ [c]
+               
+deSpace :: [Token] -> [Token]
+deSpace = filter (\t -> t /= TokenSpace)
+
+opToString :: Operator -> String
+opToString Plus = "+"
+opToString Minus = "-"
+opToString Times = "*"
+opToString Div = "/"
+
+showTokenContent :: Token -> String
+showTokenContent (TokenOperator op) = opToString op
+showTokenContent (TokenIdentifier str) = str
+showTokenContent (TokenNumber i) = show i
+showTokenContent (TokenSpace) = " "
+
+token :: Token
+token = TokenIdentifier "x"
+
+data Expression
+
+parse :: [Token] -> Expression
+parse = undefined
+
+evaluate :: Expression -> Double
+evaluate = undefined
+
 
 main :: IO ()
 main = do
-    print $ operatorToCharacter Plus
-    print $ operatorToCharacter Minus
-    print $ operatorToCharacter Times
-    print $ operatorToCharacter Div
+    print $ tokenize " 1 + 4 / x"
+    print $ tokenize "2+2+2*x+1"
